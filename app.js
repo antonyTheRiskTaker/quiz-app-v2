19ccc1e21a627169a41229e981c8d069a6b60ad9
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-10-19 11:41:35 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Import 3rd-party modules
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+// Import custom modules from library
+const handlers = require('./lib/handlers');
+
+const app = express();
+
+// Configure handlebars view engine
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Route handlers
+app.get('/', handlers.home);
+
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+});
